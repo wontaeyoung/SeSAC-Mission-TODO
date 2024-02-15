de9ab51e00b74fe87b10b06e5dcf7ca06cf16694
Taeyoung Won <hexter1994@gmail.com>
2024-02-15 09:32:39 +0900

    [Feat] AddTodoViewController 구현

--- SeSAC-Mission-TODO/Presenter/Home/Add/AddTodoViewController.swift
+++ SeSAC-Mission-TODO/Presenter/Home/Add/AddTodoViewController.swift
@@ -20,3 +20,106 @@ enum TodoConfiguration: String, CaseIterable {
     return self.rawValue
   }
 }
+
+final class AddTodoViewController: BaseViewController, ViewModelController {
+  
+  // MARK: - UI
+  private let cardView = CardView()
+  private let titleTextField = UITextField().configured {
+    $0.placeholder = "제목"
+    $0.borderStyle = .none
+  }
+  private let divider = Divider()
+  private let memoTextView = UITextView().configured {
+    $0.backgroundColor = .clear
+  }
+  
+  private lazy var configTableView = UITableView().configured {
+    $0.keyboardDismissMode = .onDrag
+    $0.register(TodoConfigTableViewCell.self, forCellReuseIdentifier: TodoConfigTableViewCell.identifier)
+    $0.delegate = self
+    $0.dataSource = self
+  }
+  
+  // MARK: - Property
+  let viewModel: AddTodoViewModel
+  
+  // MARK: - Initialzier
+  init(viewModel: AddTodoViewModel) {
+    self.viewModel = viewModel
+    
+    super.init()
+  }
+  
+  // MARK: - Life Cycle
+  override func setHierarchy() {
+    view.addSubviews(cardView, configTableView)
+    cardView.addSubviews(titleTextField, divider, memoTextView)
+  }
+  
+  override func setAttribute() {
+    setNavigationItems()
+  }
+  
+  override func setConstraint() {
+    cardView.snp.makeConstraints { make in
+      make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+    }
+    
+    titleTextField.snp.makeConstraints { make in
+      make.top.horizontalEdges.equalTo(cardView).inset(8)
+    }
+    
+    divider.snp.makeConstraints { make in
+      make.top.equalTo(titleTextField.snp.bottom).offset(8)
+      make.horizontalEdges.equalTo(cardView).inset(8)
+    }
+    
+    memoTextView.snp.makeConstraints { make in
+      make.top.equalTo(divider.snp.bottom).offset(8)
+      make.bottom.horizontalEdges.equalTo(cardView).inset(8)
+      make.height.equalTo(150)
+    }
+    
+    configTableView.snp.makeConstraints { make in
+      make.top.equalTo(cardView.snp.bottom).offset(8)
+      make.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide).inset(16)
+    }
+  }
+  
+  // MARK: - Method
+  private func setNavigationItems() {
+    let cancelBarButton = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(cancelBarButtonTapped))
+    let addBarButton = UIBarButtonItem(title: "추가", style: .plain, target: self, action: #selector(addBarButtonTapped))
+    
+    navigationItem.leftBarButtonItem = cancelBarButton
+    navigationItem.rightBarButtonItem = addBarButton
+  }
+  
+  @objc private func cancelBarButtonTapped() {
+    viewModel.dismiss()
+  }
+  
+  @objc private func addBarButtonTapped() {
+    viewModel.dismiss()
+  }
+}
+
+extension AddTodoViewController: TableControllable {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return TodoConfiguration.allCases.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(
+      withIdentifier: TodoConfigTableViewCell.identifier,
+      for: indexPath
+    ) as! TodoConfigTableViewCell
+    
+    let data = viewModel.todoItem.current
+    let config = TodoConfiguration.allCases[indexPath.row]
+    cell.updateUI(with: data, config: config)
+    
+    return cell
+  }
+}
