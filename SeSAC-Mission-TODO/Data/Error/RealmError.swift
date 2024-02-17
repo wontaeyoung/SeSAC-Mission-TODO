68c7e4919eae42c731a814bc2ff46334f36a76db
Taeyoung Won <hexter1994@gmail.com>
2024-02-17 17:07:10 +0900

    [Add] Realm 변경 감지 에러 케이스 추가

--- SeSAC-Mission-TODO/Data/Error/RealmError.swift
+++ SeSAC-Mission-TODO/Data/Error/RealmError.swift
@@ -9,17 +9,21 @@ import KazUtility
 
 enum RealmError: AppError {
   case getRealmFailed
+  case observedChangeError(error: Error)
   
   var logDescription: String {
     switch self {
       case .getRealmFailed:
         return "Realm 데이터베이스 객체를 생성하는데 실패했습니다."
+        
+      case .observedChangeError(let error):
+        return "감지된 변경사항에서 에러가 발생했습니다. \(error.localizedDescription)"
     }
   }
   
   var alertDescription: String {
     switch self {
-      case .getRealmFailed:
+      case .getRealmFailed, .observedChangeError:
         return "정보를 가져오는데 실패했어요. 문제가 지속되면 개발자에게 문의해주세요."
     }
   }
