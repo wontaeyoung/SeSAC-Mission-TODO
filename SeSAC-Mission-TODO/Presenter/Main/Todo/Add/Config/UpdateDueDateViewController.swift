e9c5f9ded5f251cf400cc11d9d6ddbde7e5f9d47
Taeyoung Won <hexter1994@gmail.com>
2024-02-18 01:48:51 +0900

    [Feat] 클래스 기본 구현

---
+++ SeSAC-Mission-TODO/Presenter/Main/Todo/Add/Config/UpdateDueDateViewController.swift
@@ -0,0 +1,45 @@
+//
+//  UpdateDueDateViewController.swift
+//  SeSAC-Mission-TODO
+//
+//  Created by 원태영 on 2/15/24.
+//
+
+import UIKit
+import KazUtility
+
+final class UpdateDueDateViewController: BaseViewController {
+  
+  // MARK: - UI
+  private let datePicker = UIDatePicker().configured {
+    $0.datePickerMode = .date
+    $0.preferredDatePickerStyle = .automatic
+  }
+  
+  // MARK: - Property
+  var updateDueDateAction: (Date) -> Void
+  
+  // MARK: - Initializer
+  init(updateDateAction: @escaping (Date) -> Void) {
+    self.updateDueDateAction = updateDateAction
+    
+    super.init()
+  }
+  
+  // MARK: - Life Cycle
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    
+    updateDueDateAction(datePicker.date)
+  }
+  
+  override func setHierarchy() {
+    view.addSubview(datePicker)
+  }
+  
+  override func setConstraint() {
+    datePicker.snp.makeConstraints { make in
+      make.edges.equalTo(view.safeAreaLayoutGuide)
+    }
+  }
+}
