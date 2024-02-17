336a85b20b39f18af49bf788a7d37263483df067
Taeyoung Won <hexter1994@gmail.com>
2024-02-17 18:03:27 +0900

    [Fix] 기존 데이터 로직 삭제, Realm 관련 로직으로 변경

--- SeSAC-Mission-TODO/Presenter/Main/Todo/HomeViewModel.swift
+++ SeSAC-Mission-TODO/Presenter/Main/Todo/HomeViewModel.swift
@@ -9,60 +9,55 @@ import Foundation
 import KazUtility
 import RealmSwift
 
-final class HomeViewModel: ViewModel {
+final class HomeViewModel: RealmViewModel {
   
   // MARK: - Property
   weak var coordinator: HomeCoordinator?
   private let repository: any TodoItemRepository
   
+  // MARK: - Model
+  var model: Results<TodoItem>
+  var observe: ((Results<TodoItem>) -> Void)?
+  var notificationToken: NotificationToken?
+  
   // MARK: - Initializer
   init(coordinator: HomeCoordinator? = nil, repository: any TodoItemRepository) {
     self.coordinator = coordinator
     self.repository = repository
-    self.todoItems = repository.fetch()
+    self.model = repository.fetch()
   }
   
-  // MARK: - Bindable
-  private var todoItems: Results<TodoItem>
+  deinit {
+    notificationToken?.invalidate()
+  }
+}
+
+extension HomeViewModel {
   
-  // MARK: - Method
   func filter(by state: TodoItem.State) -> [TodoItem] {
     switch state {
       case .today:
-        return todoItems.filter { $0.isToday }
+        return model.filter { $0.isToday }
       case .plan:
-        return todoItems.filter { !$0.isDone }
+        return model.filter { !$0.isDone }
       case .all:
-        return todoItems.map { $0 }
+        return model.map { $0 }
       case .flag:
-        return todoItems.filter { $0.isFlag }
+        return model.filter { $0.isFlag }
       case .done:
-        return todoItems.filter { $0.isDone }
+        return model.filter { $0.isDone }
     }
   }
   
   func filteredCount(by state: TodoItem.State) -> Int {
     return filter(by: state).count
   }
+}
+
+extension HomeViewModel {
   
   @MainActor
   func showAddTodoView() {
     coordinator?.showAddTodoView()
   }
-}
-
-enum TodoNotificationNameKey: String, NotificationNameKey {
-  
-  case todoItemAdded = "TodoItemAdded"
-}
-
-enum TodoNotificationInfoKey: String, NotificationInfoKey {
-  
-  case todoItem
-}
-
-struct TodoNotificationInfo: NotificationInfo {
-  
-  var key: TodoNotificationInfoKey
-  var value: Any
 }
