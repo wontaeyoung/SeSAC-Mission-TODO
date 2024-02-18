e2fc2f199c4c0a79bd60c0b655ff253912aeaafa
Taeyoung Won <hexter1994@gmail.com>
2024-02-19 00:25:40 +0900

    [Fix] name으로 초기화하는 convenience init 추가

--- SeSAC-Mission-TODO/Data/Model/TodoTag.swift
+++ SeSAC-Mission-TODO/Data/Model/TodoTag.swift
@@ -17,8 +17,13 @@ final class TodoTag: Object, RealmModel {
     }
   }
   
-  @Persisted(primaryKey: true) var name: String
-  
   /// 이 태그 레코드(인스턴스)를 포함하는 TodoItem과 역방향 관계 형성
   let todoItems = LinkingObjects(fromType: TodoItem.self, property: TodoItem.Column.tags.name)
+  @Persisted(primaryKey: true) var name: String
+  
+  convenience init(name: String) {
+    self.init()
+    
+    self.name = name
+  }
 }
