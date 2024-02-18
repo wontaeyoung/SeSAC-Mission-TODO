bfa275bdb8858f290e4c07db8b79fcc3833b8c7d
Taeyoung Won <hexter1994@gmail.com>
2024-02-19 02:25:14 +0900

    [Fix] 태그 상태 업데이트 구현

--- SeSAC-Mission-TODO/Presenter/Main/Cell/TodoTagCollectionViewCell.swift
+++ SeSAC-Mission-TODO/Presenter/Main/Cell/TodoTagCollectionViewCell.swift
@@ -12,12 +12,12 @@ import SnapKit
 final class TodoTagCollectionViewCell: BaseCollectionViewCell {
   
   // MARK: - UI
-  private let tagButton = UIButton().configured { button in
-    button.configuration = .filled().configured {
-      $0.buttonSize = .mini
-      $0.cornerStyle = .medium
-    }
+  private lazy var tagButton = UIButton().configured {
+    $0.addTarget(self, action: #selector(tagButtonTapped), for: .touchUpInside)
   }
+  
+  // MARK: - Property
+  var updateTagAction: (() -> Void)?
   
   // MARK: - Life Cycle
   override func setHierarchy() {
@@ -31,8 +31,20 @@ final class TodoTagCollectionViewCell: BaseCollectionViewCell {
   }
   
   // MARK: - Method
-  func updateUI(with data: String) {
-    tagButton.configuration?.title = "#\(data)"
+  func updateUI(with data: String, isTagging: Bool, row: Int) {
+    let config: UIButton.Configuration = isTagging ? .filled() : .gray()
+    
+    tagButton.configuration = config.configured {
+      $0.title = "#\(data)"
+      $0.buttonSize = .small
+      $0.cornerStyle = .small
+    }
+    
+    tagButton.tag = row
+  }
+  
+  // MARK: - Selector
+  @objc private func tagButtonTapped() {
+    updateTagAction?()
   }
 }
-
