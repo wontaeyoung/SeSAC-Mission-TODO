1127161879c26ebc60c799f70fc1ae135fce41af
Taeyoung Won <hexter1994@gmail.com>
2024-02-19 11:52:21 +0900

    [Feat] Realm Collection 커스텀 필터 메서드 구현

--- SeSAC-Mission-TODO/Data/Model/RealmModel.swift
+++ SeSAC-Mission-TODO/Data/Model/RealmModel.swift
@@ -5,9 +5,40 @@
 //  Created by 원태영 on 2/17/24.
 //
 
+import Foundation
 import RealmSwift
 
 protocol RealmModel: Object {
   
   associatedtype Column: RawRepresentable where Column.RawValue == String
+}
+
+extension Results where Element: RealmModel {
+  
+  enum Operator {
+    enum Logical: String {
+      case and = "&&"
+      case or = "||"
+    }
+    
+    enum Comparison: String {
+      case less = "<"
+      case lessEqual = "<="
+      case equal = "=="
+      case greaterEqual = ">="
+      case greater = ">"
+    }
+  }
+  
+  func `where`(
+    comparison: Operator.Comparison = .equal,
+    column: Element.Column,
+    value: any CVarArg
+  ) -> Results<Element> {
+    
+    let predicateFormat: String = "\(column.rawValue) \(comparison.rawValue) %@"
+    let predicate = NSPredicate(format: predicateFormat, value)
+    
+    return self.filter(predicate)
+  }
 }
