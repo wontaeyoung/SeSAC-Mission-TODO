58d4c0981e26c3991c05c33849373273474164ca
Taeyoung Won <hexter1994@gmail.com>
2024-02-19 16:27:10 +0900

    [Feat] 이미지 추가 기능 구현

--- SeSAC-Mission-TODO/Presenter/Main/Todo/Add/AddTodoViewController.swift
+++ SeSAC-Mission-TODO/Presenter/Main/Todo/Add/AddTodoViewController.swift
@@ -10,6 +10,246 @@ import KazUtility
 import SnapKit
 import RealmSwift
 
+final class AddTodoViewController: BaseViewController, ViewModelController {
+  
+  // MARK: - UI
+  private let cardView = CardView()
+  
+  private lazy var titleTextField = UITextField().configured {
+    $0.placeholder = "제목"
+    $0.borderStyle = .none
+    $0.autocapitalizationType = .none
+    $0.autocorrectionType = .no
+    $0.spellCheckingType = .no
+    $0.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+  }
+  
+  private let divider = Divider()
+  
+  private let memoTextView = UITextView().configured {
+    $0.backgroundColor = .clear
+    $0.autocapitalizationType = .none
+    $0.autocorrectionType = .no
+    $0.spellCheckingType = .no
+  }
+  
+  private lazy var configTableView = UITableView().configured {
+    $0.keyboardDismissMode = .onDrag
+    $0.register(TodoConfigTableViewCell.self, forCellReuseIdentifier: TodoConfigTableViewCell.identifier)
+    $0.delegate = self
+    $0.dataSource = self
+    $0.isScrollEnabled = false
+  }
+  
+  private let photoImageView = UIImageView().configured { $0.contentMode = .scaleAspectFit }
+  
+  // MARK: - Property
+  let viewModel: AddTodoViewModel
+  
+  private var titleText: String {
+    return titleTextField.text ?? ""
+  }
+  
+  private var memo: String {
+    return memoTextView.text ?? ""
+  }
+  
+  // MARK: - Initialzier
+  init(viewModel: AddTodoViewModel) {
+    self.viewModel = viewModel
+    
+    super.init()
+    
+    NotificationManager.shared.add(self, key: TodoNotification.NameKey.updateConfig) { [weak self] notification in
+      guard let self else { return }
+      
+      if let flag = notification.userInfo?[TodoNotification.InfoKey.flag.name] as? Bool {
+        viewModel.updateFlag(with: flag)
+        reloadConfig(with: .flag)
+      }
+      
+      if let priority = notification.userInfo?[TodoNotification.InfoKey.priority.name] as? Int {
+        viewModel.updatePriority(with: priority)
+        reloadConfig(with: .priority)
+      }
+    }
+  }
+  
+  deinit {
+    NotificationManager.shared.remove(self)
+  }
+  
+  // MARK: - Life Cycle
+  override func setHierarchy() {
+    view.addSubviews(cardView, configTableView, photoImageView)
+    cardView.addSubviews(titleTextField, divider, memoTextView)
+  }
+  
+  override func setAttribute() {
+    setNavigationItems()
+  }
+  
+  override func setConstraint() {
+    cardView.snp.makeConstraints { make in
+      make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+    }
+    
+    titleTextField.snp.makeConstraints { make in
+      make.top.horizontalEdges.equalTo(cardView).inset(8)
+    }
+    
+    divider.snp.makeConstraints { make in
+      make.top.equalTo(titleTextField.snp.bottom).offset(8)
+      make.horizontalEdges.equalTo(cardView).inset(8)
+    }
+    
+    memoTextView.snp.makeConstraints { make in
+      make.top.equalTo(divider.snp.bottom).offset(8)
+      make.bottom.horizontalEdges.equalTo(cardView).inset(8)
+      make.height.equalTo(150)
+    }
+    
+    configTableView.snp.makeConstraints { make in
+      make.top.equalTo(cardView.snp.bottom).offset(8)
+      make.horizontalEdges.equalTo(view).inset(16)
+    }
+    
+    photoImageView.snp.makeConstraints { make in
+      make.top.equalTo(configTableView.snp.bottom).offset(8)
+      make.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide).inset(16)
+      make.size.equalTo(200)
+    }
+  }
+  
+  // MARK: - Method
+  private func setNavigationItems() {
+    let cancelBarButton = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(cancelBarButtonTapped))
+    let addBarButton = UIBarButtonItem(title: "추가", style: .plain, target: self, action: #selector(addBarButtonTapped))
+    
+    navigationItem.leftBarButtonItem = cancelBarButton
+    navigationItem.rightBarButtonItem = addBarButton
+    
+    updateAddButtonEnabled(isTitleEmpty: titleText.isEmpty)
+  }
+  
+  private func updateAddButtonEnabled(isTitleEmpty: Bool) {
+    navigationItem.rightBarButtonItem?.isEnabled = !isTitleEmpty
+  }
+  
+  private func showUpdateConfigView(with config: TodoConfiguration) {
+    switch config {
+      case .dutDate:
+        viewModel.showUpdateDueDateView(current: viewModel.object.dueDate, config: config) { [weak self] date in
+          guard let self else { return }
+          
+          viewModel.updateDueDate(with: date)
+          reloadConfig(with: config)
+        }
+        
+      case .flag:
+        viewModel.showUpdateFlagView(current: viewModel.object.isFlag, config: config)
+      
+      case .tag:
+        viewModel.showUpdateTagView(config: config, tags: viewModel.object.tags, delegate: self)
+        
+      case .priority:
+        viewModel.showUpdatePriorityView(current: viewModel.object.priority, config: config)
+        
+      case .addImage:
+        showPhotoSelectionActionSheet()
+    }
+  }
+  
+  private func reloadConfig(with config: TodoConfiguration) {
+    configTableView.reloadRow(row: config.row)
+  }
+  
+  private func showPhotoSelectionActionSheet() {
+    let alert = UIAlertController(title: "사진을 어떻게 가져올까요?", message: nil, preferredStyle: .actionSheet)
+      .setAction(title: GetPhotoSelection.camera.title, style: .default) { self.showGetPhotoView(selection: .camera) }
+      .setAction(title: GetPhotoSelection.album.title, style: .default) { self.showGetPhotoView(selection: .album) }
+      .setAction(title: GetPhotoSelection.web.title, style: .default)
+      .setCancelAction()
+    
+    present(alert, animated: true)
+  }
+  
+  private func showGetPhotoView(selection: GetPhotoSelection) {
+    guard !(selection == .web) else { return }
+    
+    let imagePicker = UIImagePickerController().configured {
+      $0.delegate = self
+      if selection == .camera { $0.sourceType = .camera }
+    }
+    
+    present(imagePicker, animated: true)
+  }
+  
+  // MARK: - Selector
+  @objc private func cancelBarButtonTapped() {
+    viewModel.dismiss()
+  }
+  
+  @objc private func addBarButtonTapped() {
+    viewModel.updateTitle(with: titleText)
+    viewModel.updateMemo(with: memo)
+    viewModel.add()
+    viewModel.dismiss()
+    if let image = photoImageView.image {
+      viewModel.writeImage(
+        image: image,
+        router: .write(
+          fileName: viewModel.object.id.stringValue,
+          fileExtension: .jpg,
+          level: .middle
+        )
+      )
+    }
+  }
+  
+  @objc private func textFieldDidChange() {
+    updateAddButtonEnabled(isTitleEmpty: titleText.isEmpty)
+  }
+}
+
+// MARK: - Image Picker Delegate
+extension AddTodoViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    if let pickedImage = info[.originalImage] as? UIImage {
+      photoImageView.image = pickedImage
+    }
+    
+    dismiss(animated: true)
+  }
+}
+
+// MARK: - Table Delegate
+extension AddTodoViewController: TableControllable {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return TodoConfiguration.allCases.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(
+      withIdentifier: TodoConfigTableViewCell.identifier,
+      for: indexPath
+    ) as! TodoConfigTableViewCell
+    
+    let data = viewModel.object
+    let config = TodoConfiguration.allCases[indexPath.row]
+    cell.updateUI(with: data, config: config)
+    
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let config = TodoConfiguration.allCases[indexPath.row]
+    showUpdateConfigView(with: config)
+  }
+}
+
 enum TodoConfiguration: Int, CaseIterable {
   
   case dutDate
@@ -57,197 +297,6 @@ enum GetPhotoSelection: Int, CaseIterable {
   
   var index: Int {
     return self.rawValue
-  }
-}
-
-final class AddTodoViewController: BaseViewController, ViewModelController {
-  
-  // MARK: - UI
-  private let cardView = CardView()
-  
-  private lazy var titleTextField = UITextField().configured {
-    $0.placeholder = "제목"
-    $0.borderStyle = .none
-    $0.autocapitalizationType = .none
-    $0.autocorrectionType = .no
-    $0.spellCheckingType = .no
-    $0.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
-  }
-  
-  private let divider = Divider()
-  
-  private let memoTextView = UITextView().configured {
-    $0.backgroundColor = .clear
-    $0.autocapitalizationType = .none
-    $0.autocorrectionType = .no
-    $0.spellCheckingType = .no
-  }
-  
-  private lazy var configTableView = UITableView().configured {
-    $0.keyboardDismissMode = .onDrag
-    $0.register(TodoConfigTableViewCell.self, forCellReuseIdentifier: TodoConfigTableViewCell.identifier)
-    $0.delegate = self
-    $0.dataSource = self
-    $0.isScrollEnabled = false
-  }
-  
-  private let photoImageView = UIImageView()
-  
-  // MARK: - Property
-  let viewModel: AddTodoViewModel
-  
-  private var titleText: String {
-    return titleTextField.text ?? ""
-  }
-  
-  private var memo: String {
-    return memoTextView.text ?? ""
-  }
-  
-  // MARK: - Initialzier
-  init(viewModel: AddTodoViewModel) {
-    self.viewModel = viewModel
-    
-    super.init()
-    
-    NotificationManager.shared.add(self, key: TodoNotification.NameKey.updateConfig) { [weak self] notification in
-      guard let self else { return }
-      
-      if let flag = notification.userInfo?[TodoNotification.InfoKey.flag.name] as? Bool {
-        viewModel.updateFlag(with: flag)
-        reloadConfig(with: .flag)
-      }
-      
-      if let priority = notification.userInfo?[TodoNotification.InfoKey.priority.name] as? Int {
-        viewModel.updatePriority(with: priority)
-        reloadConfig(with: .priority)
-      }
-    }
-  }
-  
-  deinit {
-    NotificationManager.shared.remove(self)
-  }
-  
-  // MARK: - Life Cycle
-  override func setHierarchy() {
-    view.addSubviews(cardView, configTableView)
-    cardView.addSubviews(titleTextField, divider, memoTextView)
-  }
-  
-  override func setAttribute() {
-    setNavigationItems()
-  }
-  
-  override func setConstraint() {
-    cardView.snp.makeConstraints { make in
-      make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
-    }
-    
-    titleTextField.snp.makeConstraints { make in
-      make.top.horizontalEdges.equalTo(cardView).inset(8)
-    }
-    
-    divider.snp.makeConstraints { make in
-      make.top.equalTo(titleTextField.snp.bottom).offset(8)
-      make.horizontalEdges.equalTo(cardView).inset(8)
-    }
-    
-    memoTextView.snp.makeConstraints { make in
-      make.top.equalTo(divider.snp.bottom).offset(8)
-      make.bottom.horizontalEdges.equalTo(cardView).inset(8)
-      make.height.equalTo(150)
-    }
-    
-    configTableView.snp.makeConstraints { make in
-      make.top.equalTo(cardView.snp.bottom).offset(8)
-      make.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide).inset(16)
-    }
-  }
-  
-  // MARK: - Method
-  private func setNavigationItems() {
-    let cancelBarButton = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(cancelBarButtonTapped))
-    let addBarButton = UIBarButtonItem(title: "추가", style: .plain, target: self, action: #selector(addBarButtonTapped))
-    
-    navigationItem.leftBarButtonItem = cancelBarButton
-    navigationItem.rightBarButtonItem = addBarButton
-    
-    updateAddButtonEnabled(isTitleEmpty: titleText.isEmpty)
-  }
-  
-  private func updateAddButtonEnabled(isTitleEmpty: Bool) {
-    navigationItem.rightBarButtonItem?.isEnabled = !isTitleEmpty
-  }
-  
-  private func showUpdateConfigView(with config: TodoConfiguration) {
-    switch config {
-      case .dutDate:
-        viewModel.showUpdateDueDateView(current: viewModel.object.dueDate, config: config) { [weak self] date in
-          guard let self else { return }
-          
-          viewModel.updateDueDate(with: date)
-          reloadConfig(with: config)
-        }
-        
-      case .flag:
-        viewModel.showUpdateFlagView(current: viewModel.object.isFlag, config: config)
-      
-      case .tag:
-        viewModel.showUpdateTagView(config: config, tags: viewModel.object.tags, delegate: self)
-        
-      case .priority:
-        viewModel.showUpdatePriorityView(current: viewModel.object.priority, config: config)
-        
-      default:
-        break
-    }
-  }
-  
-  private func reloadConfig(with config: TodoConfiguration) {
-    configTableView.reloadRow(row: config.row)
-  }
-  
-  // MARK: - Selector
-  @objc private func cancelBarButtonTapped() {
-    viewModel.dismiss()
-  }
-  
-  @objc private func addBarButtonTapped() {
-    viewModel.updateTitle(with: titleText)
-    viewModel.updateMemo(with: memo)
-    viewModel.add()
-    viewModel.dismiss()
-  }
-  
-  @objc private func textFieldDidChange() {
-    updateAddButtonEnabled(isTitleEmpty: titleText.isEmpty)
-  }
-}
-
-// MARK: - Table Delegate
-extension AddTodoViewController: TableControllable {
-  
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return TodoConfiguration.allCases.count
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(
-      withIdentifier: TodoConfigTableViewCell.identifier,
-      for: indexPath
-    ) as! TodoConfigTableViewCell
-    
-    let data = viewModel.object
-    let config = TodoConfiguration.allCases[indexPath.row]
-    cell.updateUI(with: data, config: config)
-    
-    return cell
-  }
-  
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    let config = TodoConfiguration.allCases[indexPath.row]
-    showUpdateConfigView(with: config)
   }
 }
 
