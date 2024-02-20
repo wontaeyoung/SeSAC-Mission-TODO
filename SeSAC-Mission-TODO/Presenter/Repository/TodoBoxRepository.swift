954403b484a51c420d6b1ca97b52bf331c102d60
Taeyoung Won <hexter1994@gmail.com>
2024-02-20 21:04:37 +0900

    [Add] 레포지토리 프로토콜 정의

---
+++ SeSAC-Mission-TODO/Presenter/Repository/TodoBoxRepository.swift
@@ -0,0 +1,20 @@
+//
+//  TodoBoxRepository.swift
+//  SeSAC-Mission-TODO
+//
+//  Created by 원태영 on 2/20/24.
+//
+
+import RealmSwift
+
+protocol TodoBoxRepository {
+  
+  // MARK: - Method
+  func create(with item: TodoBox) throws
+  func fetch() -> Results<TodoBox>
+  func fetchSorted(by column: TodoBox.Column, ascending: Bool) -> Results<TodoBox>
+  func update(to item: TodoBox, with value: [TodoBox.Column: Any]) throws
+  func updateColumnAll(with objects: Results<TodoBox>, in column: TodoBox.Column, value: Any) throws
+  func delete(with item: TodoBox) throws
+  func deleteCollection(with objeects: Results<TodoBox>) throws
+}
