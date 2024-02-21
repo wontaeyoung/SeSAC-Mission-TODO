f1b6a9e8b5161c33e729d5a9d0a326289de6eed6
Taeyoung Won <hexter1994@gmail.com>
2024-02-22 00:26:37 +0900

    [Feat] 목록 할 일 리스트 뷰모델 클래스 구현

---
+++ SeSAC-Mission-TODO/Presenter/Main/Todo/BoxTodoList/BoxTodoListViewModel.swift
@@ -0,0 +1,75 @@
+//
+//  BoxTodoListViewModel.swift
+//  SeSAC-Mission-TODO
+//
+//  Created by 원태영 on 2/21/24.
+//
+
+import Foundation
+import KazUtility
+import RealmSwift
+
+final class BoxTodoListViewModel: RealmObjectViewModel {
+  
+  typealias ObjectType = TodoBox
+  
+  // MARK: - Property
+  weak var coordinator: HomeCoordinator?
+  private let repository: any TodoBoxRepository
+  
+  // MARK: - Model
+  var object: ObjectType
+  var bindAction: ((ObjectType) -> Void)?
+  var notificationToken: NotificationToken?
+  
+  // MARK: - Initializer
+  init(coordinator: HomeCoordinator? = nil, repository: any TodoBoxRepository, box: TodoBox) {
+    self.coordinator = coordinator
+    self.repository = repository
+    self.object = box
+  }
+  
+  deinit {
+    notificationToken?.invalidate()
+  }
+  
+  // MARK: - Method
+  func loadImage(router: PhotoFileRouter) -> Data? {
+    guard router.fileExist else { return nil }
+    
+    return FileManager.default.contents(atPath: router.filePath)
+  }
+  
+  func updateIsDone(with data: TodoItem) {
+    do {
+      try repository.updateListItem(to: object, at: data) {
+        $0.isDone.toggle()
+      }
+    } catch {
+      let error: RealmError = .updateFailed(error: error)
+      LogManager.shared.log(with: error, to: .local)
+      Task { await coordinator?.showErrorAlert(error: error) }
+    }
+  }
+  
+  func deleteItem(with data: TodoItem) {
+    do {
+      try repository.deleteListItem(to: object, at: data)
+    } catch {
+      let error: RealmError = .updateFailed(error: error)
+      LogManager.shared.log(with: error, to: .local)
+      Task { await coordinator?.showErrorAlert(error: error) }
+    }
+  }
+}
+
+@MainActor
+extension BoxTodoListViewModel {
+  func showMakeTodoView() {
+    coordinator?.combineMakeTodoFlow(makeTodoStyle: .add(box: object))
+  }
+  
+  func showUpdateBoxView() {
+    coordinator?.showMakeBoxView(makeBoxStyle: .update(box: object))
+  }
+}
