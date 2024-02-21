8318490246691f59ce59687747c7575a6c2fe0bf
Taeyoung Won <hexter1994@gmail.com>
2024-02-22 00:23:41 +0900

    [Feat] update, delete 메서드 정의

--- SeSAC-Mission-TODO/Presenter/Repository/TodoBoxRepository.swift
+++ SeSAC-Mission-TODO/Presenter/Repository/TodoBoxRepository.swift
@@ -14,8 +14,13 @@ protocol TodoBoxRepository {
   func append(with item: TodoItem, to box: TodoBox) throws
   func fetch() -> Results<TodoBox>
   func fetchSorted(by column: TodoBox.Column, ascending: Bool) -> Results<TodoBox>
+  func update(with box: TodoBox) throws
+  func update(with box: TodoBox, updating action: (TodoBox) -> Void) throws
   func update(to item: TodoBox, with value: [TodoBox.Column: Any]) throws
+  func updateListItem(to box: TodoBox, at item: TodoItem, updating action: (TodoItem) -> Void) throws
   func updateColumnAll(with objects: Results<TodoBox>, in column: TodoBox.Column, value: Any) throws
   func delete(with item: TodoBox) throws
+  func deleteListItem(to box: TodoBox, at item: TodoItem) throws
+  func disconnectListItem(to box: TodoBox, at item: TodoItem) throws
   func deleteCollection(with objeects: Results<TodoBox>) throws
 }
